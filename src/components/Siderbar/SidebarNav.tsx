0979419c0263c02f97e47f45fb0ca0c21e3a2720
lcspaiva87 <lcspaiva87@gmail.com>
2022-03-16 16:43:23 -0300

    navLink correção

--- src/components/Siderbar/SidebarNav.tsx
+++ src/components/Siderbar/SidebarNav.tsx
@@ -8,13 +8,13 @@ export function SlidebarNav() {
         <>
             <Stack spacing="12" align="flex-start">
                 <NavSection title="Geral">
-                    <NavLink children="Dashboard" icon={RiDashboardLine} href="/dashboard" />
-                    <NavLink children="Usuários" icon={RiContactsLine}  href="/users"/>
+                    <NavLink icon={RiDashboardLine} href="/dashboard" >Dashboard</NavLink>
+                    <NavLink icon={RiContactsLine} href="/users" >Usuários</NavLink>
                 </NavSection>
 
                 <NavSection title="Automação">
-                    <NavLink children="Formulários" icon={RiInputMethodLine} href="/forms" />
-                    <NavLink children="Automação" icon={RiGitMergeLine} href="/automation" />
+                    <NavLink icon={RiInputMethodLine} href="/forms" >Formulários</NavLink>
+                    <NavLink icon={RiGitMergeLine} href="/automation" > Automação</NavLink>
                 </NavSection>
             </Stack>
 
